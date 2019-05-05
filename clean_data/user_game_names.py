7b272aea422e2240cfc91ccde15378f6c6f511f0
Daniel Liang <dzliang100@gmail.com>
2019-05-05 19:08:33 -0400

    Changed formatting of names file

--- clean_data/user_game_names.py
+++ clean_data/user_game_names.py
@@ -1,5 +1,6 @@
 import ast
 import json
+import pandas as pd
 
 filepath = './user_items.json'
 writefilepath = './user_game_names.csv'
@@ -7,14 +8,16 @@ writefilepath = './user_game_names.csv'
 # max_game_id = 99920 for 100k cap
 # 				= 19990 for 20k cap
 # total cap is 530720
-data = []
+
+df = pd.DataFrame(columns = ['userid', 'game_names'])
 
 with open(filepath) as f:
+	i = 0
 	for line in f:
 		l = ast.literal_eval(line)	# Convert single quotes in json to proper double quotes
-
+	
 		# Don't consider a user that doesn't own any games
-		if int(l["items_count"]) > 0:		
+		if int(l["items_count"]) > 0:
 			games = l["items"]
 			games_filter = []
 			for g in games:
@@ -23,10 +26,11 @@ with open(filepath) as f:
 					game_id = int(g["item_id"])
 					if game_id <= 100000: 		# Set limit on games due to space
 						games_filter.append(g["item_name"])
-			data.append({l["user_id"]: games_filter})
+			
+			if len(games_filter) >= 1:
+				df.loc[i] = pd.Series({'userid': l["user_id"], 'game_names': games_filter})
+				i += 1
 
-with open(writefilepath, 'w') as outfile:
-	outfile.write(
-		'[' +
-		',\n'.join(json.dumps(i) for i in data) +
-		']\n')
+
+
+df.to_csv(writefilepath, header=False, index=False)
